d1fe101c280666f3a64d2b289ffa762241a05398
zanxi <consultantweb25@gmail.com>
2023-04-22 22:04:02 +0300

    добавлены файлы

---
+++ sql_script_del__19-04-2023 11-50-02.sql
@@ -0,0 +1,9 @@
+DROP TABLE RIndication;
+DROP TABLE RTransmitBuffer;
+DROP TABLE RMilkSettings;
+DROP TABLE RFeedVisit;
+DROP TABLE RMilkVisit;
+DROP TABLE RVisitTab;
+DROP TABLE RFeedSettings;
+DROP TABLE RGWSettings;
+  
